3a01c10d91011744ff66036a5bd21883560ee598
Ric-olive <ric-olive@hotmail.com>
2018-09-17 20:15:56 +0200

    Uploaded index and Js file

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" href="Img/title.ico">
+	<title>R&O connect</title>
+
+	<link rel="stylesheet" type="text/css" href="Include/bootstrap-4.1.3/dist/css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="Include/bootstrap-4.1.3/dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="Css/style.css">
+	<link rel="stylesheet" type="text/css" href="Css/styles.css">
+	<link rel="stylesheet" type="text/css" href="Css/sex.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Kaushan+Script|Sawarabi+Mincho">
+
+
+</head>
+
+
+<body>
+	
+	<div class="container-fluid" id="full-width">
+		<div class="col-lg-12 background">
+			<div class="parent_video">
+			<video autoplay muted loop class="my_video" poster="Img/Palm_Trees/Palm_Trees.jpg">
+   				<source src="Img/Palm_Trees/Palm_Trees.mp4" type="video/mp4" class="my_video_video">
+   				<source src="Img/Palm_Trees/Palm_Trees.webm" type="video/webm" class="my_video_video">
+   			</video>
+   			</div>
+
+   			<header class="my_header">
+				<?php include('Include/Header/navbar.php') ?>
+			</header>
+   		</div>
+
+   		<div class="col-lg-12">
+   			 <div class="col footer_titre_slogan">
+        		<h2 class="logo_home"><span class="logo_first_letter">R</span> <small class="logo_second_letter">&</small> <span class="logo_third_letter">O</span></h2>
+        		<h4 class="home_logo_slogan">Elegance in our feet <span><img class="icon_logo" src="Img/title.ico" rel="shortcut icon"></span></h4>
+    		</div>
+   		</div>
+
+   		<div class="col shop_online_btn">
+   		<a href="">
+   			SHOP ONLINE</a>
+   		</div>
+	</div>
+
+	<div class="container">
+		<div class="col-lg-5 img_select">
+			<img src="Img/male_shoe.jpg" class="img-thumbnail">
+			<figcaption><a href="" class="caption" id="txt1">Male</a></figcaption>
+			
+		</div>
+		<div class="col-lg-5 img_select">
+			<img src="Img/women_shoe.jpg" class="img-thumbnail">
+			<figcaption><a href="" class="caption" id="txt2">Female</a></figcaption>
+		</div>
+		
+	</div>
+
+
+	<!-- <footer>
+		<?php include('Include/Footer/footer.php') ?>
+	</footer> -->
+
+
+<script type="text/javascript" src="Js/script.js"></script>
+<script type="text/javascript" src="Js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="Include/bootstrap-4.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="Include/bootstrap-4.1.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
